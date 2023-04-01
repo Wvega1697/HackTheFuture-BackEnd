8adbfa784b7779b26ecf67b277cae89de82ff48a
WVEGA1697 <williamsv1697@gmail.com>
2023-04-01 08:43:23 -0600

    ADD
    Initial commit

---
+++ src/main/java/com/wvega/EcoIndustriaApplication.java
@@ -0,0 +1,13 @@
+package com.wvega;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoIndustriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcoIndustriaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wvega/EcoIndustriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wvega;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoIndustriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
